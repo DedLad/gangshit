2cd7b7d2d1d5a3905a7616b2986b9865a83ff9fb
DedLad <adheshathrey2004@gmail.com>
2024-12-31 01:58:33 +0530

    build file

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm -rf *
-git clone https://github.com/DedLad/gangshit.git
+git clone https://github.com/DedLad/internethome.git
 cp -r gangshit/site .
 curl -L https://github.com/anna-ssg/anna/releases/download/v3.0.0/anna_Linux_x86_64.tar.gz >anna_Linux_x86.tar.gz
 tar -xvzf anna_Linux_x86.tar.gz
